f1a38f261aa0ee92e5c683b5f9b89e75a8a251e0
Amaury Lopez Diaz <amaury.lopezdiaz@gmail.com>
2021-12-02 15:26:00 -0500

    problem 3 solved

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -48,7 +48,12 @@ let evensArr = []
 */
 
 //Code Here
-
+for (i = 0; i < nums.length; i++){
+  if(nums[i] % 2 === 0) {
+    evensArr.push(nums[i]);
+  }console.log(nums[i])
+}
+ console.log(evensArr)
 
 
 ////////// PROBLEM 5 //////////
